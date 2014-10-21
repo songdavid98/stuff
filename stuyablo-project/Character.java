fdf75024043f45865caa1d151f5a2d40c41fbd64
James Chin <james.chin@stuypulse.com>
2014-10-21 15:20:58 -0400

    Added Character class

---
+++ stuyablo-project/Character.java
@@ -0,0 +1,32 @@
+public class Character {
+
+  //variables
+  String name;
+  int health;
+  int attack;
+  int defense;
+  int intelligence;
+   
+  String[] inventory;
+   
+  public Character(String n) {
+    name = n;
+  }
+  
+  public String toString() {
+    String s = "";
+    s += "Name: " + name + "\n";
+    s += "Health: " + health + "\n";
+    s += "Intelligence: " + intelligence + "\n";
+    s += "Attack: " + attack + "\n";
+    s += "Defencse: " + defense;
+    return s;
+  }
+  
+  public static void main(String[] args) {
+    Character c = new Character("James");
+    
+    System.out.println(c.toString());
+  }
+  
+}
