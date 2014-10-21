0ad6ff3342515f6cdaa67c555352a0dd5248df9d
songdavid98 <songdavid98@gmail.com>
2014-10-21 15:26:00 -0400

    general structure updated

--- stuyablo-project/Game.java
+++ stuyablo-project/Game.java
@@ -1,5 +1,16 @@
-public class Game {
+import java.util.*;
 
+public class Game {
+  boolean win;
   int day;
 
+  public static void main(String[] args) {
+    System.out.println();//introduce the game and ask character for name and character choice
+    Scanner scan = new Scanner(System.in);
+    while (win) {
+    
+
+    }
+
+  }
 }
