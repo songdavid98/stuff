b63c1f217085e5ee7a1a0f7bedb741b2128177c0
James Chin <james.chin@stuypulse.com>
2014-10-22 15:29:40 -0400

    added beginning of game

--- stuyablo-project/Game.java
+++ stuyablo-project/Game.java
@@ -1,13 +1,34 @@
+import java.util.*;
+
 public class Game {
 
   int day;
-
+  
   public static void main(String[] args) {
-    Character c = new Character("James");
-    Character n = new Character("idek");
+  
+    Scanner s = new Scanner(System.in);
+    String t;
+  
+    System.out.println("*Boom* *Crash*");
+    System.out.println("Your plane crashed.");
+    System.out.println("You're on a beach.");
+    System.out.println("With an ocean as far as the eye can see on one side.");
+    System.out.println("And a huge forest on the other.");
+    System.out.println("Where is everyone?");
+    System.out.println("Press enter to continue:");
     
-    System.out.println(c.toString());
-    System.out.println(n.toString());
+    System.out.print("");
+    t = s.nextLine();
+    
+    System.out.println("You are alone on an island. Or are you?");
+    System.out.println("Your mission is to survive, and hopefully, escape this isolated world.");
+    System.out.println("You have five days worth of food, and a backpack (inventory).");
+    System.out.println("Good luck!");
+    System.out.println();
+    System.out.print("What is your name? \n");
+    t = s.nextLine();
+    Character c = new Character(t);
+  
   }  
 
 }
